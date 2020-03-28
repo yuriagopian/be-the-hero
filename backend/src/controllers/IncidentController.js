7540a217a803ab08f328207e59aa852d57dd1c4d
yuri agopian <yuri.agopian@hotmail.com>
2020-03-28 02:20:37 -0300

    finalização do mobile

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -26,7 +26,7 @@ module.exports = {
             .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
-            .select(['* incidents',
+            .select([' incidents.* ',
                 'ongs.name',
                 'ongs.email',
                 'ongs.whatsapp',
